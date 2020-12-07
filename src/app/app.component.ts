26b6c9e28c04d06c804aab1fd867c54b070b0d97
Blessing <bntoko@gmail.com>
2020-12-07 17:10:42 +0200

    removed unused module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { timeStamp } from 'console';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
